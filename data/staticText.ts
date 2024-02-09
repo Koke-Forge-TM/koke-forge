4429c559ca20bfe837903cecfeedabdd02d55fa7
DannelCu <danneldavidr@gmail.com>
2024-02-08 23:31:41 -0500

    some real text hiding unnecessary stuff

---
+++ data/staticText.ts
@@ -0,0 +1,6 @@
+export const staticText = {
+    heroTitleEs: "KokeForge Team",
+    heroTitleEn: "KokeForge Team",
+    heroSubEs: "Forjando tus sueños, haz crecer tu negocio con nuestros productos de software.",
+    heroSubEn: "Forging your dreams, grow your business with our software solutions."
+}
