44d26a86234947ae69a6be55d805872b02d8861c
YzE <pluischacon@gmail.com>
2024-02-10 00:56:30 -0500

    project cards  display fix

--- components/projects/card/ProjectCard.tsx
+++ components/projects/card/ProjectCard.tsx
@@ -19,40 +19,76 @@ export default function ProjectCard(props: ProjectCardProps) {
     return (
         <>
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                {
+                    props.index % 2 == 0 ?
+                        <>
+                            <div
+                                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                                data-aos="fade-up">
+                                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={img.src} width={540}
+                                       height={405} alt={img.altEn}/>
+                            </div>
+                            <div
+                                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                                data-aos="fade-right">
+                                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                                    <Link href={project.blueTitle.url}
+                                          className="font-architects-daughter text-xl text-purple-600 mb-2">
+                                        {project.blueTitle.titleEn}
+                                    </Link>
+                                    <h3 className="h3 mb-3">{project.titleEn}</h3>
+                                    <p className="text-xl text-gray-400 mb-4">{project.subtitleEn}</p>
+                                    <ul className="text-lg text-gray-400 -mb-2">
+                                        {
+                                            features && features.length > 0 ?
+                                                features.map((feature: ProjectFeature, index: number) => (
+                                                    <li className="flex items-center mb-2">
+                                                        <ProjectFeat key={index} feature={feature}/>
+                                                    </li>
+                                                ))
+                                                :
+                                                <div></div>
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <div
+                                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                                data-aos="fade-up">
+                                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={img.src} width={540}
+                                       height={405} alt={img.altEn}/>
+                            </div>
+                            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                                 data-aos="fade-left">
+                                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                                    <Link href={project.blueTitle.url}
+                                          className="font-architects-daughter text-xl text-purple-600 mb-2">
+                                        {project.blueTitle.titleEn}
+                                    </Link>
+                                    <h3 className="h3 mb-3">{project.titleEn}</h3>
+                                    <p className="text-xl text-gray-400 mb-4">{project.subtitleEn}</p>
+                                    <ul className="text-lg text-gray-400 -mb-2">
+                                        {
+                                            features && features.length > 0 ?
+                                                features.map((feature: ProjectFeature, index: number) => (
+                                                    <li className="flex items-center mb-2">
+                                                        <ProjectFeat key={index} feature={feature}/>
+                                                    </li>
+                                                ))
+                                                :
+                                                <div></div>
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </>
+                }
 
-                <div
-                    className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
-                    data-aos="fade-up">
-                    <Image className="max-w-full mx-auto md:max-w-none h-auto" src={img.src} width={540} height={405}
-                           alt={img.altEn}/>
-                </div>
 
-                <div
-                    className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-                    data-aos={props.index % 2 == 0 ? "fade-right" : "fade-left"}>
-                    <div className={props.index % 2 == 0 ? "md:pr-4 lg:pr-12 xl:pr-16" : "md:pl-4 lg:pl-12 xl:pl-16"}>
-                        <Link href={project.blueTitle.url}
-                            className="font-architects-daughter text-xl text-purple-600 mb-2">
-                            {project.blueTitle.titleEn}
-                        </Link>
-                        <h3 className="h3 mb-3">{project.titleEn}</h3>
-                        <p className="text-xl text-gray-400 mb-4">{project.subtitleEn}</p>
-                        <ul className="text-lg text-gray-400 -mb-2">
-                            {
-                                features && features.length > 0 ?
-                                    features.map((feature: ProjectFeature, index: number) => (
-                                        <li className="flex items-center mb-2">
-                                            <ProjectFeat key={index} feature={feature}/>
-                                        </li>
-                                    ))
-                                    :
-                                    <div></div>
-                            }
-                        </ul>
-                    </div>
-                </div>
             </div>
         </>
     )
-
 }
--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -1,4 +1,4 @@
-import {projects,projectSection} from "@/data/projects"
+import {projects, projectSection} from "@/data/projects"
 // import {getAllProjects} from "@/lib/requestProjects"
 import type {Project} from "@/schemas/projects"
 
@@ -26,7 +26,6 @@ export default function Projects() {
 
                     {/* Items */}
                     <div className="grid gap-20">
-
                         {
                             projects && projects.length > 0 ?
                                 projects.map((project: Project, index: number) => (
@@ -35,49 +34,6 @@ export default function Projects() {
                                 :
                                 <p>NO PROJECTS</p>
                         }
-
-                        {/* 1st item */}
-
-
-                        {/*/!* 2nd item *!/*/}
-                        {/*<div className="md:grid md:grid-cols-12 md:gap-6 items-center">*/}
-                        {/*  /!* Image *!/*/}
-                        {/*  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">*/}
-                        {/*    <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={540} height={405} alt="Features 02" />*/}
-                        {/*  </div>*/}
-                        {/*  /!* Content *!/*/}
-                        {/*  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">*/}
-                        {/*    <div className="md:pl-4 lg:pl-12 xl:pl-16">*/}
-                        {/*      <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>*/}
-                        {/*      <h3 className="h3 mb-3">Keep projects on schedule</h3>*/}
-                        {/*      <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>*/}
-                        {/*      <ul className="text-lg text-gray-400 -mb-2">*/}
-                        {/*        <li className="flex items-center mb-2">*/}
-                        {/*          <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">*/}
-                        {/*            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />*/}
-                        {/*          </svg>*/}
-                        {/*          <span>Duis aute irure dolor in reprehenderit</span>*/}
-                        {/*        </li>*/}
-                        {/*        <li className="flex items-center mb-2">*/}
-                        {/*          <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">*/}
-                        {/*            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />*/}
-                        {/*          </svg>*/}
-                        {/*          <span>Excepteur sint occaecat</span>*/}
-                        {/*        </li>*/}
-                        {/*        <li className="flex items-center">*/}
-                        {/*          <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">*/}
-                        {/*            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />*/}
-                        {/*          </svg>*/}
-                        {/*          <span>Amet consectetur adipiscing elit</span>*/}
-                        {/*        </li>*/}
-                        {/*      </ul>*/}
-                        {/*    </div>*/}
-                        {/*  </div>*/}
-                        {/*</div>*/}
-
-                        {/* 3rd item */}
-
-
                     </div>
 
                 </div>
